7ff58ab788baa972bdafa3b1f7d1a7c90e8bdb5d
yonatan gershfeld <yonatan310867@gmail.com>
2023-03-26 15:16:52 +0300

    add img prev image slider

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const nextE1 =document.querySelector(".next");
 
+const prevE1 = document.querySelector(".prev");
+
 const imgs = document.querySelectorAll("img");
 
 
@@ -12,9 +14,16 @@ nextE1.addEventListener("click",() =>{
     updateimg()
 })
 
+prevE1.addEventListener("click", () =>{
+    currentimg--
+    updateimg()
+
+})
 function updateimg(){
     if(currentimg > imgs.length){
         currentimg = 1;
+    }else if(currentimg < 1){
+        currentimg = imgs.length;
     }
     imagecontainerE1.style.transform = `translateX(-${(currentimg - 1) * 500}px)`;
 }
